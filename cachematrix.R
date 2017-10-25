be6c07a91d111396cf0e5ae74ead5c82e41ed3e9
nephthali <n.djabontchounda@cineca.it>
2017-10-25 09:48:53 +0200

    Cache matrix done

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -12,4 +20,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        is.square <- function(M){
+                return( is.matrix(M) && (nrow(M) == ncol(M)))
+        }
+        if(is.square(data)){
+                #message("execution of solve function")
+                inv <- solve(data)
+        } 
+        
+        #else {
+        #message("execution of ginv function")
+        ## library MASS must be load to get the Generalized Inverse of Matrix (ginv) function
+        #inv <- ginv(data)    
+        #}
+        x$setinv(inv)
+        inv
 }
